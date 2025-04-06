b4eff98cc7f2074ca82e8333d49997160aef8ad8
themestermim <themestermim@gmail.com>
2025-04-06 08:57:38 +0330

    font bug fixed

--- components/Home/Hero/index.vue
+++ components/Home/Hero/index.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="relative">
         <!-- right squares -->
-         <div class="absolute -right-10 top-16 size-46 border border-gray-500 -rotate-[31deg] before:content-[''] before:absolute before:size-46 before:right-0 before:top-0 before:border before:border-gray-900 before:-rotate-[18deg]"></div>
+        <div
+            class="hidden lg:block absolute -right-10 top-16 size-46 border border-gray-500 -rotate-[31deg] before:content-[''] before:absolute before:size-46 before:right-0 before:top-0 before:border before:border-gray-900 before:-rotate-[18deg]">
+        </div>
+
+        <div class="container">
+            <Swiper class="w-full" v-bind="settings">
+                <SwiperSlide>
+                    <h2 class="text-base text-white font-extrabold block text-center">ساندویچ پانل سقفی</h2>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+
+        <!-- left squares -->
+        <div
+            class="hidden lg:block absolute -left-10 top-16 size-46 border border-gray-500 -rotate-[31deg] before:content-[''] before:absolute before:size-46 before:right-0 before:top-0 before:border before:border-gray-900 before:-rotate-[18deg]">
+        </div>
     </div>
 </template>
 
@@ -9,4 +24,8 @@
 defineOptions({
     name: "HomeHero",
 })
+
+const settings = {
+    slidesPerView: 1,
+}
 </script>
