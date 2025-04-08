aeab7730e23e378ade01b7dd510319e2afd51537
themestermim <themestermim@gmail.com>
2025-04-08 13:29:21 +0330

    website logo added.

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,5 +1,14 @@
 <template>
-<header class="text-white">هدر</header>
+    <header :class="{
+        'sticky top-0': true,
+    }">
+        <div class="container grid grid-cols-2 h-14">
+            <!-- logo -->
+             <div class="flex items-center justify-end h-full">
+                <NuxtImg src="" />
+             </div>
+        </div>
+    </header>
 </template>
 
 <script setup>
