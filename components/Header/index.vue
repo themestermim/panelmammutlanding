c3f9617df40d49063ae81803cc8da23b1c1cccb3
themestermim <themestermim@gmail.com>
2025-04-12 14:49:55 +0330

    start bottom sheet

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -9,43 +9,61 @@
         }">
             <!-- mobile menu & phone -->
             <div class="flex items-center gap-8 order-1 md:order-3 md:justify-end">
-                <button type="button" role="button" @click="layoutStore.toggleMenu" aria-label="منو" class="text-white cursor-pointer md:hidden">
+                <button type="button" role="button" @click="layoutStore.toggleMenu" aria-label="منو"
+                        class="text-white cursor-pointer md:hidden">
                     <svg class="size-5">
-                        <use :href="isMenuOpen ? '#svg-close' : '#svg-hamburger-menu'" />
+                        <use :href="isMenuOpen ? '#svg-close' : '#svg-hamburger-menu'"/>
                     </svg>
                 </button>
                 <!-- phone number -->
                 <a href="tel:02145510"
-                    class="text-white cursor-pointer md:h-10 md:rounded-3xl md:bg-primary-50 md:px-4 md:flex md:items-center md:gap-2 md:text-primary-500 lg:gap-4 lg:px-6 lg:tracking-wider">
-                    <span class="hidden md:block text-sm font-semibold">021-45510</span>
+                   class="text-white cursor-pointer md:h-10 md:rounded-3xl md:bg-primary-50 md:px-4 md:flex md:items-center md:gap-2 md:text-primary-500 lg:gap-4 lg:px-6 lg:tracking-wider">
+                    <span class="hidden md:block text-sm font-semibold lg:text-base lg:font-bold">021-45510</span>
                     <svg class="size-5">
-                        <use href="#svg-call" />
+                        <use href="#svg-call"/>
                     </svg>
                 </a>
             </div>
             <!-- logo -->
             <div class="flex items-center justify-end h-full order-2 md:order-1 md:justify-start">
-                <NuxtImg src="/assets/img/shared/logo.png" alt="ماموت ساختمان" class="h-8 md:h-10 lg:h-12 xl:h-14" />
+                <NuxtImg src="/assets/img/shared/logo.png" alt="ماموت ساختمان" :class="{
+                    'transition-all': true,
+                    'h-8 md:h-10 lg:h-12 xl:h-14': !minimizeHeader,
+                    'h-7 md:h-9 lg:h-11 xl:h-12': minimizeHeader,
+                }"/>
             </div>
             <!-- desktop navbar -->
             <div class="hidden md:flex col-span-3 order-2 md:justify-center md:h-full md:items-center md:gap-2">
-                <Nav />
+                <Nav/>
             </div>
         </div>
 
-        <!-- mobile navbar -->
-        <Transition name="fade">
-            <div v-if="isMenuOpen" :class="{
-                'fixed bottom-0 top-auto right-0 w-full bg-gray-50 p-4 md:hidden h-52': true,
-            }">
-                <Nav />
-            </div>
-        </Transition>
+        <HLTransitionRoot
+            :show="isMenuOpen"
+            as="template"
+        >
+            <HLDialog @close="layoutStore.closeMenu">
+                <HLTransitionChild
+                    enter="transition-all ease-in-out duration-300"
+                    enter-from="translate-y-52"
+                    enter-to="translate-y-0"
+                    leave="transition-all ease-in-out duration-300"
+                    leave-from="translate-y-0"
+                    leave-to="translate-y-52"
+                    as="div"
+                    class="fixed bottom-0 top-auto right-0 w-full bg-gray-50 p-4 z-[5] md:hidden h-52 rounded-t-2xl overflow-auto transition-all ease-in-out"
+                >
+                    <HLDialogPanel>
+                        <Nav/>
+                    </HLDialogPanel>
+                </HLTransitionChild>
+            </HLDialog>
+        </HLTransitionRoot>
     </header>
 </template>
 
 <script setup>
-import { useLayout } from '~/store/layout';
+import {useLayout} from '~/store/layout';
 import Nav from "./Nav.vue";
 
 defineOptions({
@@ -61,8 +79,6 @@ const BUFFER = 10;
 
 const handleScroll = () => {
     const scrollY = window.scrollY;
-
-    // console.log(window.scrollY)
 
     if (!minimizeHeader.value && scrollY > SCROLL_THRESHOLD + BUFFER) {
         minimizeHeader.value = true;
@@ -95,15 +111,3 @@ onUnmounted(() => {
 });
 
 </script>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.15s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-    opacity: 0;
-}
-</style>
